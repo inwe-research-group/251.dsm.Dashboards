4169a6084ec678542fbe9ee71a9ae43efc8a4117
inwe-research-group <zmamanir@unmsm.edu.pe>
2025-06-27 18:31:39 -0500

    add data package

---
+++ app/src/main/java/com/example/dashboards/data/Datos.kt
@@ -0,0 +1,6 @@
+package com.example.dashboards.data
+
+data class Datos(
+    val label: String,
+    val value: Float
+)
---
+++ app/src/main/java/com/example/dashboards/data/DatosDashboard.kt
@@ -0,0 +1,13 @@
+package com.example.dashboards.data
+
+object DatosDashboard {
+    val datos:List<Datos> = listOf(
+        Datos("Lun",9f),
+        Datos("Mar",8f),
+        Datos("Mie",10f),
+        Datos("Ju",3f),
+        Datos("Vi",8f),
+        Datos("Sab",3f),
+        Datos("Dom",7f)
+    )
+}
